78ecc604f2a948d14aeb95f00f93e723ab81d1f7
Andrey <a.akushevich@nyxbull.com>
2018-11-18 19:38:34 +0300

    top

---
+++ script.js
@@ -0,0 +1,28 @@
+if (window.XMLHttpRequest)
+{ // для IE7+, Firefox, Chrome
+  xmlhttp = new XMLHttpRequest();}
+else
+{ // для IE6, IE5
+  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xmlhttp.open("GET","storage.xml",false);
+xmlhttp.send();
+
+xmlDoc = xmlhttp.responseXML;
+
+document.write("<table class=table>");
+for( i=1;i<101;i++)
+{
+    document.write("<tr>");
+    document.write("<td class=num>"+ i +"</td><td><a href=#  class=name></a></td><td class=author></td><td class=year></td>");
+    document.write("</tr>");
+}
+document.write("</table>");
+
+for( i=0;i<100;i++)
+{
+  document.getElementsByClassName("name")[i].innerHTML = xmlDoc.getElementsByTagName("title")[i].childNodes[0].nodeValue;
+  document.getElementsByClassName("author")[i].innerHTML = xmlDoc.getElementsByTagName("author")[i].childNodes[0].nodeValue;
+  document.getElementsByClassName("year")[i].innerHTML = xmlDoc.getElementsByTagName("year")[i].childNodes[0].nodeValue;
+}
