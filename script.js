9d485289e55d930657c2af1934a18b4fe69b9cf3
Andrey <new.box41@gmail.com>
2018-11-24 08:25:51 +0300

    new

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ else
   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 
+
 xmlhttp.open("GET","storage.xml",false);
 xmlhttp.send();
 
@@ -15,7 +16,7 @@ document.write("<table class=table>");
 for( i=1;i<101;i++)
 {
     document.write("<tr>");
-    document.write("<td class=num>"+ i +"</td><td><a href=#  class=name></a></td><td class=author></td><td class=year></td>");
+    document.write("<td class=num>"+ i +"</td><td><span class=name onclick=newPage("+i+")></span></td><td class=author></td><td class=year></td>");
     document.write("</tr>");
 }
 document.write("</table>");
@@ -26,3 +27,7 @@ for( i=0;i<100;i++)
   document.getElementsByClassName("author")[i].innerHTML = xmlDoc.getElementsByTagName("author")[i].childNodes[0].nodeValue;
   document.getElementsByClassName("year")[i].innerHTML = xmlDoc.getElementsByTagName("year")[i].childNodes[0].nodeValue;
 }
+function newPage(num) {
+
+    var newWin = window.open(book.html, winName);
+  }
